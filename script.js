06f2d0bf821b2fa1e642aea82e086864264b480c
Steven McDaniel <79287709+StevenMcDaniel@users.noreply.github.com>
2021-02-20 15:23:18 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const menu = document.getElementsByClassName("menu");
+
+menu[0].onclick = function() {
+  window.location = "index.html";
+};
+menu[1].onclick = function() {
+  window.location = "games.html";
+};
+menu[2].onclick = function() {
+  window.location = "contacts.html";
+};
+menu[3].onclick = function() {
+  window.location = "announcements.html";
+};
+menu[4].onclick = function() {
+  window.location = "atc.html";
+};
