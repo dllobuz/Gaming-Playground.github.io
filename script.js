1a1498cd13241275319de76fbc16bfe968a4a07b
Steven McDaniel <79287709+StevenMcDaniel@users.noreply.github.com>
2021-02-22 15:34:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menu = document.getElementsByClassName("menu");
+const gameBtn = document.getElementsByClassName("gameBtn");
 
 menu[0].onclick = function() {
   window.location = "index.html";
@@ -15,3 +16,7 @@ menu[3].onclick = function() {
 menu[4].onclick = function() {
   window.location = "atf.html";
 };
+
+gameBtn[0].onclick = function() {
+  window.location = "Sealed in Stone/index.html"
+}
